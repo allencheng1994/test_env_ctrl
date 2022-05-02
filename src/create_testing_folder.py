ed2ed8bfa03c89eedfde2df83d93d8ed569e5a02
chengcl-MacBookAIR <chengcl1994@gmail.com>
2022-05-02 08:39:54 +0800

    [feat] recursion version

--- src/create_testing_folder.py
+++ src/create_testing_folder.py
@@ -1,8 +1,43 @@
 from pathlib import Path
-
-def create_folders(projet_path: Path, case_num: str, config_file: Path) -> None:
-    pass
+import json
 
 
-if __name__ == '__main__':
-    pass
+def create_folders(project_path: Path, config_file: Path) -> None:
+    with open(config_file, "r", encoding="utf-8") as f:
+        config_data = json.load(f)
+    struct_list = restruct_testcase(config_data)
+    for struct in struct_list:
+        make_folders(project_path, struct)
+
+
+def restruct_testcase(config: dict) -> list[dict]:
+    last_case_num = 0
+    result = []
+    for struct in config:
+        go_to_create = (
+            config.get(struct).get("quant")
+            if config.get(struct).get("quant") != None
+            else 1
+        )
+        new_struct = config.get(struct)
+        if new_struct.get("quant") != None:
+            new_struct.pop("quant")
+        for i in range(go_to_create):
+            result.append({"testcase_" + str(last_case_num + i + 1): new_struct})
+        last_case_num += go_to_create
+    return result
+
+
+def make_folders(project_path: Path, item_struct: dict) -> None:
+    for item in item_struct:
+        created_path = project_path.joinpath(item)
+        if not created_path.exists():
+            created_path.mkdir()
+        if item_struct.get(item) != {} and type(item_struct.get(item) != "dict"):
+            make_folders(created_path, item_struct.get(item))
+
+
+if __name__ == "__main__":
+    test_place = Path(r"/Users/chengcl1994/Documents/code/python/test_env_ctrl")
+    config = test_place.joinpath("config.json")
+    create_folders(test_place, config)
