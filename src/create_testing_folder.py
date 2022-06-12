483ccc4737c5cfdbfe6e55d0b1e4547facdf1b84
chengcl-MacBookAIR <chengcl1994@gmail.com>
2022-06-12 14:39:29 +0800

    [doc] Add the docstrings in create_testing_folder

--- src/create_testing_folder.py
+++ src/create_testing_folder.py
@@ -4,7 +4,21 @@ import json
 DEFAULT_CONFIG = Path(__file__).parent.joinpath("default_config.json")
 
 
-def create_folders_with_default(project_path: Path, quant: int = 1):
+def create_folders_with_default(project_path: Path, quant: int = 1) -> None:
+    """Create the folders by using the default settings
+
+    Create the folders by using the default settings.
+    The default folder's layout is stored in src/default_config.json
+
+    Args:
+        project_path(Path): The path you want to place your tested folder
+        quant(int): Because of unique layout in defult, create the samples by using
+        this argument.
+
+    Returns:
+        None: It won't retun anything, but it will create a bunch of folders
+
+    """
     with open(DEFAULT_CONFIG, "r", encoding="utf-8") as f:
         config_data = json.load(f)
     config_data["struct_1"]["quant"] = quant
@@ -14,6 +28,20 @@ def create_folders_with_default(project_path: Path, quant: int = 1):
 
 
 def create_folders_with_config(project_path: Path, config_file: Path) -> None:
+    """Create the folders with the settings in the configure file
+
+    Create the folder by using custom configure file. You can reference
+    src/default_config.json to create your own configure file.
+
+    Args:
+        project_path(:obj:`pathlib.Path`): The path you want to place your tested folder
+        config_file(:obj:`pathlib.Path`): The configure file
+
+    Returns:
+        None: It won't retun anything
+
+    """
+
     with open(config_file, "r", encoding="utf-8") as f:
         config_data = json.load(f)
     struct_list = restruct_testcase(config_data)
@@ -22,12 +50,25 @@ def create_folders_with_config(project_path: Path, config_file: Path) -> None:
 
 
 def restruct_testcase(config: dict) -> list[dict]:
+    """Convert the 'quant' to the structure
+
+    In this function, we are going to convert 'quant' in config into the
+    'struct'
+
+    Args:
+        config(dict): The folder's layers config. You can reference 
+        src/default_config.json
+
+    Return:
+        list: A list of the folder with layers written in dictionary type
+
+    """
     last_case_num = 0
     result = []
     for struct in config:
         go_to_create = (
             config.get(struct).get("quant")
-            if config.get(struct).get("quant") != None
+            if config.get(struct).get("quant") != None and config.get(struct) != 0
             else 1
         )
         new_struct = config.get(struct)
@@ -40,6 +81,12 @@ def restruct_testcase(config: dict) -> list[dict]:
 
 
 def make_folders(project_path: Path, item_struct: dict) -> None:
+    """Create the folders by using a specific dictionary
+    
+    This function can help your create a bunch of folders by using a specific 
+    dictionary. The dictionary template is in src/default_config.json
+
+    """
     for item in item_struct:
         created_path = project_path.joinpath(item)
         if not created_path.exists():
